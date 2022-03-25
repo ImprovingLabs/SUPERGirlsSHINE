2a6c3a59877bcfa2e936891be947b1bb7da10861
devlinliles <devlin.liles@improving.com>
2022-03-25 07:05:26 -0500

    Updating for basic addition

---
+++ Calculator.Tests/AdditionTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator.Domain;
+
+namespace Calculator.Tests
+{
+    [TestClass]
+    public class AdditionTests
+    {
+        [TestMethod]
+        public void CanAddTwoIntegers()
+        {
+            // Arrange
+            var calculator = new Calculator.Domain.Calculator();
+
+            // Act
+            int result = calculator.Add(7,8);
+
+            // Assert
+            Assert.AreEqual(15, result);
+        }
+    }
+}
